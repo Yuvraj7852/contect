5d42ae2eb5a3d042ae6a1dd5810c617d971d3b34
Yuvraj singh <147245929+Yuvraj7852@users.noreply.github.com>
2024-07-24 15:28:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const insta = document.querySelector("#insta");
+{
+  insta.addEventListener('click', function(){
+     window.open('https://www.instagram.com/yuvraj_singh_banjara0?igsh=MWhldTVqM2doMWlkbg==')
+  });
+
+  
+
+
+  //add
+  const link= document.querySelector("#link");
+  const you = document.querySelector("#you");
+
+  link.addEventListener('click', function(){
+     window.open('https://www.linkedin.com/in/yuvraj-singh-15')
+  });
+
+  link.addEventListener('click', function(){
+    
+  })
+
+//add
+
+
+  you.addEventListener('click', function(){
+     window.open('https://www.youtube.com/@Mr.Atom0.2')
+  });
+
+  
+
+//add
+const git = document.querySelector("#git");
+
+  git.addEventListener('click', function(){
+     window.open('https://github.com/Yuvraj7852')
+  });
+
+  const x = document.querySelector("#x");
+
+  x.addEventListener('click', function(){
+     window.open('https://x.com/yuvraj0Singh7?t=Ax_C4tPOBVZiHDZWhSGipw&s=09')
+  });
+
+ 
+
+ 
+}
